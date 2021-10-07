d513b75385ea7cf96f23130bbbe4740b60faf4a4
alxrznv <alexey.ryazanov.77@mail.ru>
2021-10-07 21:01:50 +1000

    task

--- Pflow.sol
+++ Pflow.sol
@@ -3,11 +3,11 @@ contract PhraseFlow {
     string[] public flow;
     uint public count;
 
-    function addPhrase(string _newPhrase) public {
-            flow.push(_newPhrase);
-            count = count + 1;
-    }
+    function addPhrase(string -newPhraase) public {
+        flow.push(_newPhrase);
+        count = count +1;
 
+    }
     constructor() public {
         count = 0;
     }
