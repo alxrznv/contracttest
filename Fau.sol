f725eb521ab0cfa36afc4fcbbdd37255e7039d0d
alxrznv <alexey.ryazanov.77@mail.ru>
2021-10-07 20:07:06 +1000

    new one

--- Fau.sol
+++ Fau.sol
@@ -25,7 +25,7 @@ contract Faucet is Mortal {
     receive() external payable {}
     
     function withdraw(uint withdraw_amount) public {
-        require(withdraw_amount <= 0.1 ether);
+        require(withdraw_amount <= 100000000000000000);
 
         msg.sender.transfer(withdraw_amount);
     }
